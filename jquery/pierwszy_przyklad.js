91a88d7d563dbc8a486fd9acf015d4c9141b2da4
JGluszak <jakub.gluszak08@gmail.com>
2025-09-08 08:21:17 +0200

    08.09

---
+++ jquery/pierwszy_przyklad.js
@@ -0,0 +1,42 @@
+$(function () {
+    // Funkcja ustawia styl dla strony: kolor tła, odstępy liter w nagłówku, styl linków oraz zmienia adres href na Wikipedię
+    $('#page').css('background-color', '#00FFFF');
+    $('h2').css('letter-spacing', '5px');
+    $('.link').css({
+        'color': 'white',
+        'text-decoration': 'none',
+    });
+    $('[href="#"]').attr('href', 'https://pl.wikipedia.org/wiki/J%C4%99zyk_programowania');
+});
+
+$(function () {
+    // Funkcja ustawia styl dla nieparzystych linków 
+    $('a').odd().css({
+        'color': 'blue',
+        'text-decoration': 'none',
+        'background-color': 'black',
+    })
+});
+
+$(function () {
+    // Funkcja ustawia styl dla wszystkich linków oprócz tego z id "pierwszy"
+    $('a').not('#pierwszy').css({
+
+        'text-decoration': 'underline',
+        'background-color': 'black',
+    })
+});
+
+$(function () {
+    const $lista_ul = $('ul').html();
+    console.log($lista_ul);
+    const $lista_li = $('li').html();
+    console.log($lista_li);
+});
+
+$(function () {
+    let $lista_ul = $('ul').text();
+    console.log($lista_ul);
+    let $lista_li = $('li').text();
+    console.log($lista_li + "\n");
+});
