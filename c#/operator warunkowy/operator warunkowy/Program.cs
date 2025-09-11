1a31fceea014c3ae23ad670525c00fb6acf07436
JGluszak <jakub.gluszak08@gmail.com>
2025-09-11 12:39:27 +0200

    11.09 operator warunkowy

---
+++ c#/operator warunkowy/operator warunkowy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace operator_warunkowy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int liczba = 10;
+            int liczba2;
+            liczba2 = liczba < 0 ? -1 : 1;
+            Console.WriteLine(liczba2);
+        }
+    }
+}
